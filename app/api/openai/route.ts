68db0b5f61c5f367ae0c437f2e647683d6ed98eb
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-03 05:16:43 +0530

    removed ts

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,14 +1,11 @@
-import { getServerSession } from "next-auth";
+
 import OpenAI from "openai";
-import { handler } from "../auth/[...nextauth]/route";
+
 
 const openai = new OpenAI()
 
 export async function POST(req: Request, res: Response){
-    // const session = await getServerSession(handler);
-    // if(!session){
-    //     return new Response(null, {status: 403})
-    // }
+    
     const message = (await req.json()) as {data: string}
     console.log(message)
     const completion = await openai.chat.completions.create({
